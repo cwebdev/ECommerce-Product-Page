87eab409788f3d2ac8e373d20b561169a802fe32
cwebdev <chitrangwebdev@gmail.com>
2022-03-21 14:20:37 +0530

    Code for add to cart button

--- js/script.js
+++ js/script.js
@@ -35,6 +35,57 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+////////////////
+// Code for Cart
+////////////////
+
+let productPriceInCart = 125.00;
+let productQuantityInCart = 0;
+
+let addToCartBtn = document.querySelector('.AddToCartBtnWrapper');
+addToCartBtn.onclick = function(e) {
+  let quantityValElem = document.querySelector('.QuantityValue');
+  let quantityVal = parseInt(quantityValElem.innerHTML);
+
+  if(quantityVal > 0)
+  {
+    productQuantityInCart += quantityVal;
+    // Reset quantity counter
+    let quantityValElem = document.querySelector('.QuantityValue');
+    quantityValElem.innerHTML = "0";
+  }
+
+  if(productQuantityInCart > 0)
+  {    
+    // Hide empty cart view
+    let emptyCartContent = document.querySelector('.emptyCartContent');
+    emptyCartContent.style.display = 'none';
+    
+    // Calculate and update new pricing
+    let productQuantityInCartElem = document.querySelector('.productQuantityInCart');
+    productQuantityInCartElem.innerHTML = productQuantityInCart;
+    let productTotal = productQuantityInCart * productPriceInCart;
+    
+    let productTotalInCart = document.querySelector('.productTotalInCart');
+    productTotalInCart.innerHTML = "$" + productTotal.toFixed(2);
+
+    // Show cart details
+    let cartDetailsContent = document.querySelector('.cartDetailsContent');
+    cartDetailsContent.style.display = 'block';   
+  }
+  else
+  {
+    // Hide cart details
+    let cartDetailsContent = document.querySelector('.cartDetailsContent');
+    cartDetailsContent.style.display = 'none';
+
+    // Show empty cart view
+    let emptyCartContent = document.querySelector('.emptyCartContent');
+    emptyCartContent.style.display = 'block';
+  }  
+}
+
+
 // Code for cart details popup
 let cartImgBtn = document.querySelector(".cartImageElemWrapper img");
 cartImgBtn.onclick = function(e) {
@@ -46,7 +97,7 @@ cartImgBtn.onclick = function(e) {
 }
 
 
-// Code for quantity
+// Code for cart quantity
 let plusBtn = document.querySelector('.PlusBtn');
 plusBtn.onclick = function(e) {
   let quantityValElem = document.querySelector('.QuantityValue');
@@ -65,3 +116,5 @@ negBtn.onclick = function(e) {
     quantityVal--;
   quantityValElem.innerHTML = quantityVal;
 }
+
+
